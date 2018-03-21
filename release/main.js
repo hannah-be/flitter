83b65a2a4d53bcaf498484150026716e398e42a7
Hannah Bourke <hannahbourke0@gmail.com>
2018-03-21 13:39:55 +1100

    Regex practice

--- release/main.js
+++ release/main.js
@@ -20,14 +20,25 @@ const values = {
 
 /*jshint esversion: 6 */
 const userName = prompt("Hello, what's your name?");
-document.body.innerHTML =
-  "<h1>Hello, " +
-  userName +
-  "!</h1>" +
-  "<p>" +
-  values.projectName +
-  " " +
-  values.versionNumber +
-  " accessed on: " +
-  values.currentTime +
-  "</p>";
+const phoneNumber = prompt("Hello " + userName + ", what's your phone number?");
+
+let output = "<h1>Hello, " + userName + "!</h1>";
+
+const phoneNumberPattern = /(?:\+?61|0)\s?4\s?[\d]{2}[\-\s]?\d{3}[\-\s]?\d{3}/;
+
+if (phoneNumberPattern.test(phoneNumber)) {
+  output =
+    output +
+    "<p>" +
+    values.projectName +
+    " " +
+    values.versionNumber +
+    " accessed on: " +
+    values.currentTime +
+    "</p>";
+} else {
+  output =
+    output + "<h2>That phone number is invalid: " + phoneNumber + "</h2>";
+}
+
+document.body.innerHTML = output;
